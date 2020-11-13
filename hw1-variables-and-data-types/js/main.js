f58f195cdc2ec6c8f08419e70e5016f65b137006
Melnyk <misha.melnyk94@gmail.com>
2020-11-14 01:33:04 +0200

    main js change

--- hw1-variables-and-data-types/js/main.js
+++ hw1-variables-and-data-types/js/main.js
@@ -10,3 +10,13 @@ const minPriсeGoods = Math.min(flour, butter,cream);
 console.log(minPriсeGoods);
 const sumPriceGoods = flour + butter + cream;
 console.log(sumPriceGoods);
+const sumPriceGoodsRound = Math.floor(flour) + Math.floor(butter) + Math.floor(cream);
+console.log(sumPriceGoodsRound);
+const sumRoundHundred = Math.round(sumPriceGoods / 100) * 100;
+console.log (sumRoundHundred);
+
+
+const remainderCash = cash - sumPriceGoods;
+console.log (remainderCash);
+const meanValueGoods = (sumPriceGoods / 3).toFixed(2);
+console.log(meanValueGoods);
