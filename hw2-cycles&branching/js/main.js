f02f364e3d680627e5765f8b7f90624e21a9d20f
Melnyk <misha.melnyk94@gmail.com>
2020-11-18 14:16:09 +0200

    add cycle Count sum of numbers from range

--- hw2-cycles&branching/js/main.js
+++ hw2-cycles&branching/js/main.js
@@ -1,27 +1,39 @@
-let numberOne = prompt(`Введіть перше число`); 
-// numberOne = parseInt(numberOne);
-console.log(numberOne);
+// отримання  1 числа
+let numberOneOfRange = parseInt(prompt(`Введіть перше число`)); 
+
 let audit = false;
 do{
-    if(parseInt(isNaN(numberOne))){
-    numberOne = prompt(`Це не число, введіть перше число`);
+    if(isNaN(numberOneOfRange)){
+    numberOneOfRange = parseInt(prompt(`Це не число, введіть перше число`));
     }else{
             audit = true;
         }
     }while(audit === false);
-    // console.log(audit);
-    // console.log(numberOne);
-let numberTwo = parseInt (prompt(`Введіть друге число більше за попереднє`)); 
+//отримання 2 числа
+let numberTwoOfRange = parseInt(prompt(`Введіть друге число більше за попереднє`)); 
 audit = false;
 
 do{
-    if(isNaN(numberTwo) || numberTwo <= numberOne){
-        numberTwo = parseInt (prompt(`Це не число або менше за попереднє, введіть друге число`));
+    if(isNaN(numberTwoOfRange) || numberTwoOfRange <= numberOneOfRange){
+        numberTwoOfRange = parseInt(prompt(`Це не число або менше за попереднє, введіть друге число`));// 
     }else{
         audit = true;
         }
     }while(audit === false);
 
 const skipEvenNumbers = confirm(`Пропустити парні числа?`);
-console.log(skipEvenNumbers);
 
+let sumNumberFromRange = 0;
+//складання чисел
+if(skipEvenNumbers){
+    for(let i = numberOneOfRange; i <= numberTwoOfRange; i++){
+        if(!((i % 2) === 0)){
+            sumNumberFromRange = sumNumberFromRange + i;
+        }
+    }
+}else{
+    for(let i = numberOneOfRange; i <= numberTwoOfRange; i++){
+        sumNumberFromRange = sumNumberFromRange + i;
+    }
+}
+console.log(`Сума числел : ${sumNumberFromRange}`);
