455571411375d42d919f2ea831509501fa39d5b3
Melnyk <misha.melnyk94@gmail.com>
2020-11-19 23:43:42 +0200

    correction of remarks

--- hw2-cycles&branching/js/main.js
+++ hw2-cycles&branching/js/main.js
@@ -1,39 +1,23 @@
-// отримання  1 числа
-let numberOneOfRange = parseInt(prompt(`Введіть перше число`)); 
 
-let audit = false;
-do{
-    if(isNaN(numberOneOfRange)){
-    numberOneOfRange = parseInt(prompt(`Це не число, введіть перше число`));
-    }else{
-            audit = true;
-        }
-    }while(audit === false);
-//отримання 2 числа
-let numberTwoOfRange = parseInt(prompt(`Введіть друге число більше за попереднє`)); 
-audit = false;
+    let numberOneOfRange = +prompt(`Введіть перше число`);
+    let numberTwoOfRange = +prompt(`Введіть друге число більше за попереднє`);
+    let sumNumberFromRange = 0;
 
-do{
-    if(isNaN(numberTwoOfRange) || numberTwoOfRange <= numberOneOfRange){
-        numberTwoOfRange = parseInt(prompt(`Це не число або менше за попереднє, введіть друге число`));// 
-    }else{
-        audit = true;
-        }
-    }while(audit === false);
+    while(!Number.isInteger(numberOneOfRange ) || !numberOneOfRange ) {
+    numberOneOfRange = +prompt(`Це не число, введіть перше число`);
+    }
+    
+    while(numberTwoOfRange < numberOneOfRange || !Number.isInteger(numberTwoOfRange ) || !numberTwoOfRange ) {
+        numberTwoOfRange = +prompt(`Це не число або менше за попереднє, введіть друге число`);
+    }
 
-const skipEvenNumbers = confirm(`Пропустити парні числа?`);
+    const skipEvenNumbers = confirm(`Пропустити парні числа?`);
 
-let sumNumberFromRange = 0;
-//складання чисел
-if(skipEvenNumbers){
     for(let i = numberOneOfRange; i <= numberTwoOfRange; i++){
-        if(!((i % 2) === 0)){
-            sumNumberFromRange = sumNumberFromRange + i;
+        if(skipEvenNumbers && i % 2 === 0){
+            continue;
+        }else{
+            sumNumberFromRange += i;
         }
     }
-}else{
-    for(let i = numberOneOfRange; i <= numberTwoOfRange; i++){
-        sumNumberFromRange = sumNumberFromRange + i;
-    }
-}
-console.log(`Сума числел : ${sumNumberFromRange}`);
+    console.log(`Сума числел : ${sumNumberFromRange}`);
