6a965ae59a17f7828465ad6c5e1e8f6e09485e2b
Melnyk <misha.melnyk94@gmail.com>
2020-12-10 02:18:58 +0200

    add some functions

--- hw8-class/js/main.js
+++ hw8-class/js/main.js
@@ -3,8 +3,8 @@ class Student {
         this.university = university;
         this.course = course;
         this.fullName = fullName;
-        this.marks = Array(4).forEach(element => el = Math.floor(Math.random() * 6)) ;
-
+        this.marks = Array(4).fill().map(()=> Math.floor(Math.random() * 5 + 1)) ;
+        this.status = true;
     }
 
     getInfo(){
@@ -13,6 +13,22 @@ class Student {
     
     get getmarks(){
         return this.marks;
+    }
+    set setmarks(mark){
+        this.marks.push(mark);
+    }
+    get getAverageMark(){
+        const sumNumbers = this.marks.reduce((total, number) => {
+            return total + number;
+        }, 0);
+        // console.log(sumNumbers);
+        return +(sumNumbers / this.marks.length).toFixed(2);
+    }
+    dismiss(){
+        return this.status = false;
+    }
+    recover(){
+        return this.status = true;
     }
 
 }
@@ -24,3 +40,4 @@ const bodia = new Student(`ЛНУ ім. І.Франка`, 5, `Voichyshyn Bogdan`
 console.log(misha)
 console.log(misha.getInfo());
 console.log(misha.getmarks);
+console.log(misha.getAverageMark)
