6df39d1e0542cf14d559a79c7ab56d6606b6fda3
Melnyk <misha.melnyk94@gmail.com>
2020-12-19 10:38:30 +0200

    some change

--- hw10-events/js/main.js
+++ hw10-events/js/main.js
@@ -2,7 +2,7 @@
     const surchKeyEv = function (e) {
         keys.forEach((key) =>{ 
             key.classList.remove(`playing`);
-            if (e.key.toUpperCase().charCodeAt() == key.id){
+            if (e.key.toUpperCase().charCodeAt() === +key.id){
                 key.classList.add(`playing`);
                 new Audio(
                     `src/${Math.floor(Math.random() * 6 + 1)}.mp3`
