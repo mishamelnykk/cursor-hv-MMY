16652bc14ee27b43365346fc3a02d5318e5201df
Melnyk <misha.melnyk94@gmail.com>
2020-12-25 23:38:30 +0200

    some change

--- hw12-server/js/main.js
+++ hw12-server/js/main.js
@@ -1,19 +1,36 @@
 
 const result = document.querySelector(`.result`);
 const nextPage = document.querySelector(`.buttonNext`);
-const mainButton = document.querySelector(`.buttonMain`)
-const numberfFilms = ``;
+const mainButton = document.querySelector(`.buttonMain`);
+let url = '';
 
 const showPersons = function() {
-    axios.get(`https://swapi.dev/api/films/${numberOfFilms}/`)
+    axios.get(`https://swapi.dev/api/films/${numberOfFilms}`)
         .then((res) => {
             let persons = res.data.characters;
-            persons.forEach(e => {
-                axios.get(e)
-                    .then((res) => {
-                        makePersonBox(res);
+            lang = document.querySelector(`#langValue`).value;
+            if(lang === `1`){
+                persons.forEach(e => {
+                    axios.get(e)
+                        .then((res) => {
+                            makePersonBox(res);
+                        })
                     })
-                })
+                }else{
+                    persons.forEach(e => {
+                        axios.get(e + `?format=wookiee`)
+                            .then((res) => {
+                                makePersonBoxWookiee(res);
+                            })
+                        })
+                }
+
+            // persons.forEach(e => {
+            //     axios.get(e)
+            //         .then((res) => {
+            //             makePersonBox(res);
+            //         })
+            //     })
     })
 }
 const makePersonBox = (res) => {
@@ -38,17 +55,39 @@ const makePersonBox = (res) => {
     result.append(box);
 }
 
+const makePersonBoxWookiee = (res) => {
+    const box = document.createElement(`div`);
+    box.classList.add(`personBox`);
 
-mainButton.addEventListener("click", (e) => {
+    const namePerson = document.createElement(`div`);
+    namePerson.classList.add(`namePerson`);
+    namePerson.innerHTML = `Whrascwo: ` + res.data.whrascwo;
+    box.append(namePerson);
+
+    const birthDay = document.createElement(`div`);
+    birthDay.classList.add(`birthDay`);
+    birthDay.innerHTML = `Rhahrcaoac_roworarc: ` + res.data.rhahrcaoac_roworarc;
+    box.append(birthDay);
+
+    const gender = document.createElement(`div`);
+    gender.classList.add(`genderPerson`);
+    gender.innerHTML = `Rrwowhwaworc: ` + res.data.rrwowhwaworc;
+    box.append(gender);
+
+    result.append(box);
+}
+
+
+mainButton.addEventListener("click", () => {
     const boxs = document.querySelectorAll(`.personBox`);
     if (boxs.length > 0) {
       boxs.forEach((box) => box.remove());
     }
-    numberOfFilms = document.querySelector("#selectvalue").value;
+    numberOfFilms = document.querySelector(`#selectvalue`).value;
     showPersons();
 })
 
-nextPage.addEventListener(
-    "click",
-    () => (window.location.href = "planets.html")
-  );
+nextPage.addEventListener("click", () => {
+            window.location.href = "planets.html";  
+    
+});
