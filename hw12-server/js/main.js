064f6ea0c4599e7e219cfc3663e5576060d5395a
Melnyk <misha.melnyk94@gmail.com>
2020-12-25 02:47:02 +0200

    add api requests and function - makePersonBox

---
+++ hw12-server/js/main.js
@@ -0,0 +1,32 @@
+let persons = [];
+const result = document.querySelector(".result");
+let numberOfFilms = 2;
+axios.get(`https://swapi.dev/api/films/${numberOfFilms}/`)
+    .then((res) => {
+        persons = res.data.characters;
+        persons.forEach(e => {
+            axios.get(e)
+                .then((res) => {
+                    makePersonBox(res);
+                })
+            })
+    })
+        
+const makePersonBox = (res) => {
+    const box = document.createElement("div");
+    box.classList.add(`personBox`);
+    const namePerson = document.createElement("div");
+    namePerson.classList.add("namePerson");
+    namePerson.innerHTML = res.data.name;
+    box.append(namePerson);
+
+    const birthDay = document.createElement("div");
+    birthDay.classList.add("birthDay");
+    birthDay.innerHTML = res.data.birth_year;
+    box.append(birthDay);
+
+    const gender = document.createElement("div");
+    gender.classList.add("genderPerson");
+    gender.innerHTML = res.data.gender;
+    box.append(gender);
+}
