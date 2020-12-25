7d7f59d65221707381f312b1511d44619d9d6906
Melnyk <misha.melnyk94@gmail.com>
2020-12-25 12:41:43 +0200

    some change

--- hw12-server/js/main.js
+++ hw12-server/js/main.js
@@ -1,45 +1,57 @@
-let persons = [];
-const result = document.querySelector(".result");
-let numberOfFilms = 2;
-axios.get(`https://swapi.dev/api/films/${numberOfFilms}/`)
-    .then((res) => {
-        persons = res.data.characters;
-        persons.forEach(e => {
-            axios.get(e)
-                .then((res) => {
-                    makePersonBox(res);
+// let persons = [];
+const result = document.querySelector(`.result`);
+const nextPage = document.querySelector(`.buttonNext`);
+const mainButton = document.querySelector(`.buttonMain`)
+const numberfFilms = ``;
+
+const showPersons = function() {
+    axios.get(`https://swapi.dev/api/films/${numberOfFilms}/`)
+        .then((res) => {
+            let persons = res.data.characters;
+            console.log(persons)
+            persons.forEach(e => {
+                axios.get(e)
+                    .then((res) => {
+                        makePersonBox(res);
+                    })
                 })
-            })
     })
-        
+}
 const makePersonBox = (res) => {
-    const box = document.createElement("div");
+    const box = document.createElement(`div`);
     box.classList.add(`personBox`);
 
-    const namePerson = document.createElement("div");
-    namePerson.classList.add("namePerson");
-    namePerson.innerHTML = res.data.name;
+    const namePerson = document.createElement(`div`);
+    namePerson.classList.add(`namePerson`);
+    namePerson.innerHTML = `Name: ` + res.data.name;
     box.append(namePerson);
 
-    const birthDay = document.createElement("div");
-    birthDay.classList.add("birthDay");
-    birthDay.innerHTML = res.data.birth_year;
+    const birthDay = document.createElement(`div`);
+    birthDay.classList.add(`birthDay`);
+    birthDay.innerHTML = `Birth Day: ` + res.data.birth_year;
     box.append(birthDay);
 
-    const gender = document.createElement("div");
-    gender.classList.add("genderPerson");
-    gender.innerHTML = res.data.gender;
+    const gender = document.createElement(`div`);
+    gender.classList.add(`genderPerson`);
+    gender.innerHTML = `Gender: ` + res.data.gender;
     box.append(gender);
 
     result.append(box);
 }
 
 
-
-
-const nextPage = document.querySelector(".buttonNext");
+mainButton.addEventListener("click", (e) => {
+    const boxs = document.querySelectorAll(`.personBox`);
+    if (boxs.length > 0) {
+      boxs.forEach((box) => box.remove());
+    }
+    numberOfFilms = document.querySelector("#selectvalue").value;
+    showPersons();
+})
 
 nextPage.addEventListener(
     "click",
     () => (window.location.href = "planets.html")
   );
+//   axios.get(`https://swapi.dev/api/films/4/`)
+//         .then((res) => console.log(res));
