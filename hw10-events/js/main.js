25a39f7dd0bdc60f783a431f2dec8862732999e2
Melnyk <misha.melnyk94@gmail.com>
2020-12-28 23:56:53 +0200

    add function stopAudio

--- hw10-events/js/main.js
+++ hw10-events/js/main.js
@@ -1,21 +1,32 @@
     const keys = document.querySelectorAll('.key');
+    let singl = '';
     const surchKeyEv = function (e) {
+        if (singl !== '') {stopAudio(singl);}
         keys.forEach((key) =>{ 
             key.classList.remove(`playing`);
             if (e.key.toUpperCase().charCodeAt() === +key.id){
                 key.classList.add(`playing`);
-                new Audio(
-                    `src/${Math.floor(Math.random() * 6 + 1)}.mp3`
-                ).play();
+                singl = new Audio(
+                    `src/${Math.floor(Math.random() * 6 + 1)}.mp3`);
+                singl.play();
             }
         })  
     } 
     document.addEventListener(`keydown`, surchKeyEv);
     keys.forEach((key) => key.addEventListener(`click`, function (e) {
-        keys.forEach((key) => key.classList.remove(`playing`))  ;
+        keys.forEach((key) => {
+            key.classList.remove(`playing`);
+            if (singl !== '') {stopAudio(singl);}
+         })
         key.classList.add(`playing`);
-        new Audio(
-            `src/${Math.floor(Math.random() * 6 + 1)}.mp3`
-        ).play();
+        singl = new Audio(
+            `src/${Math.floor(Math.random() * 6 + 1)}.mp3`);
+        singl.play();
+        
        
     }));
+    function stopAudio(audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    
